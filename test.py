dd8b2c38d4ad2ac62d02d1c995d8c8906c670c0d
Alex Messham <52509289+xirtam2669@users.noreply.github.com>
2025-03-27 16:33:21 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 #!/bin/python3
 from client import ElasticEDRClient
-
-
-#"alert.attributes.name:DLL" 
-
 client = ElasticEDRClient()
 
 desired_output = ["description", "query"]
 
-print(client.list_all_rules(desired_output, "alert.attributes.name:DLL"))
+print(client.list_all_rules(desired_output)
 
